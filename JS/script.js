55e3c9769968c6b027a45149009dbcce5e5500d9
bl4ck44 <hackingbl4ck44@gmail.com>
2025-03-30 19:30:39 -0500

    Version 1

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,19 @@
+// Esperar a que termine la animación del texto antes de aplicar el cambio
+setTimeout(function() {
+    document.body.classList.add("loaded"); // Añadir la clase .loaded para activar la animación
+  
+    // Redirigir a un archivo dentro del servidor después de que termine el loading
+    setTimeout(function() {
+      window.location.href = "./blog.html"; // Ajusta esta ruta con el archivo dentro del servidor
+    }, 1000); // Un segundo después de que termine la animación
+  }, 4000); // Tiempo igual al de la animación del loading (4 segundos)
+
+
+
 function toggleMenu() {
     let menu = document.querySelector(".nav__responsive-ul");
     menu.style.display = (menu.style.display === "block") ? "none" : "block";
 }
-
 
 
 
